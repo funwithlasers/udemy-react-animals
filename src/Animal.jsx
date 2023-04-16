9c4ecd08a440d234f95e0ec33c371c3eeac49486
Matt Larson <mjlarson13@gmail.com>
2023-04-15 19:16:02 -0500

    add image support and refactor

---
+++ src/Animal.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function Animal({type}) {
+
+  return (
+    <div>
+      <div >
+
+      </div>
+    </div>
+  );
+}
+
+export default Animal;
--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import AddAnimal from "./AddAnimal"
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
 
 function App() {
+  const [animals, setAnimals] = useState([]);
+
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]);
+  };
+
+  function getRandomAnimal() {
+    const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+    return animals[Math.floor(Math.random() * animals.length)];
+  }
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow type={animal} key={index} />;
+  });
+
   return (
     <div className="App">
       <header className="App-header"></header>
-      <AddAnimal></AddAnimal>
+      <button onClick={handleClick}>Add Animal</button>
+      <div>{renderedAnimals}</div>
     </div>
   );
 }
